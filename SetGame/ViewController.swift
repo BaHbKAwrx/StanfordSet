09f1ca587ea8fd52ac4c67de4cee0a042e57ba0f
BaHbKAwrx <bahbkawrx@gmail.com>
2019-04-10 17:58:16 +0300

    Some initial ui is ready

--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonsArray: [UIButton]!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var dealCardsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func buttonPushed(_ sender: UIButton) {
+    }
 
-
+    @IBAction func dealThreeCards(_ sender: UIButton) {
+    }
+    
+    @IBAction func startNewGame(_ sender: UIButton) {
+    }
+    
 }
 
